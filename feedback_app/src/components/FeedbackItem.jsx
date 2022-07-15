688bcf3db756aada6b61c74b45a17f064aa21794
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-07-15 15:36:47 +0100

    created my feedback list component

--- feedback_app/src/components/FeedbackItem.jsx
+++ feedback_app/src/components/FeedbackItem.jsx
@@ -1,25 +1,16 @@
-import React, {useState} from 'react'
-// import PropTypes from 'prop-types'
 
-const FeedbackItem = props => {
- const [rating, setRating] = useState(7)
- const [text, setText] = useState('This is an example of a feeback item')
- const handleClick= () =>{
-  setRating((prev)=>{
-   return prev + 1
-  })
-  setText('i stand here')
- }
+
+const FeedbackItem = ({item}) => {
+
   return (
     <div className="card">
-     <div className="num-display">{rating}</div>
-     <div className="text-display">{text}</div>
-     <button onClick={handleClick}>click</button>
+     <div className="num-display">{item.rating}</div>
+     <div className="text-display">{item.text}</div>
+     
     </div>
   )
 }
 
-// FeedbackItem.propTypes = {}
 
 export default FeedbackItem
 
---
+++ feedback_app/src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+	{
+		id: 1,
+		rating: 10,
+		text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis culpa quaerat quis soluta necessitatibus, esse aspernatur modi nobis nisi sunt maiores quasi rem quam voluptates labore unde tempora quos quia?',
+	},
+	{
+		id: 2,
+		rating: 6,
+		text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis culpa quaerat quis soluta necessitatibus, esse aspernatur modi nobis nisi sunt maiores quasi rem quam voluptates labore unde tempora quos quia?',
+	},
+	{
+		id: 3,
+		rating: 5,
+		text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis culpa quaerat quis soluta necessitatibus, esse aspernatur modi nobis nisi sunt maiores quasi rem quam voluptates labore unde tempora quos quia?',
+	},
+];
+
+export default FeedbackData;
