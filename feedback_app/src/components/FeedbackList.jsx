bc41d2a071beae97702987006935778b9adc0f96
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-07-16 16:36:28 +0100

    created a button for deleting of comments

--- feedback_app/src/components/FeedbackList.jsx
+++ feedback_app/src/components/FeedbackList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import FeedbackItem from './FeedbackItem'
+import PropTypes from 'prop-types'
 
 const FeedbackList = ({feedback}) => {
  
@@ -9,10 +10,19 @@ const FeedbackList = ({feedback}) => {
   return (
     <div className='feedback-list'>
       {feedback.map(item => 
-      <FeedbackItem key={item.id} item={item} />
+      <FeedbackItem key={item.id} item={item} handleDelete={(id)=> console.log(id )} />
       )}
     </div>
   )
+}
+FeedbackList.propTypes={
+ feedback: PropTypes.arrayOf(
+  PropTypes.shape({
+   id: PropTypes.number.isRequired,
+   text: PropTypes.string.isRequired,
+   rating: PropTypes.number.isRequired
+  })
+ )
 }
 
 export default FeedbackList
