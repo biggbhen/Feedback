5bc0a4763a81a69a9afe2b102c03ae3a3d5cb9f1
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-07-17 14:23:46 +0100

    created the shared button component

--- feedback_app/src/components/FeedbackForm.jsx
+++ feedback_app/src/components/FeedbackForm.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import {useState} from 'react'
 import Card from './shared/Card'
+import Button from './shared/Button'
 
 const FeedbackForm = () => {
+ const [text, setText] = useState('')
+ const handleTextChange = (e) =>{
+  setText(e.target.value)
+ }
   return (
     <Card>
-     FeedbackForm
+     <form>
+      <h2>How would you rate our services?</h2>
+      <div className="input-group">
+       <input type="text" placeholder='Write a review' value={text} onChange={handleTextChange}/>
+       <Button type='submit'>Send</Button>
+      </div>
+     </form>
     </Card>
   )
 }
