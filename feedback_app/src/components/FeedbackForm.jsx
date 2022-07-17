b43d0bbc762222f2452e58fa2cba006967285e61
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-07-17 13:47:11 +0100

    started the feedback form component

---
+++ feedback_app/src/components/FeedbackForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './shared/Card'
+
+const FeedbackForm = () => {
+  return (
+    <Card>
+     FeedbackForm
+    </Card>
+  )
+}
+
+export default FeedbackForm
