15852a37591c894ecaece9ae8bd07d3eaa173bee
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-07-21 19:17:44 +0100

    successfully routed to my about page

--- feedback_app/src/App.js
+++ feedback_app/src/App.js
@@ -1,10 +1,12 @@
 import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import FeedbackForm from './components/FeedbackForm';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import Header from './components/Header';
 import FeedbackData from './data/FeedbackData';
+import About from './pages/About';
 
 function App() {
 	const [feedback, setFeedback] = useState(FeedbackData);
@@ -19,14 +21,28 @@ function App() {
 	};
 
 	return (
-		<>
+		<Router>
 			<Header />
 			<div className='container'>
-				<FeedbackForm handleAdd={addFeedback} />
-				<FeedbackStats feedback={feedback} />
-				<FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+				<Routes>
+					<Route
+						exact
+						path='/'
+						element={
+							<>
+								<FeedbackForm handleAdd={addFeedback} />
+								<FeedbackStats feedback={feedback} />
+								<FeedbackList
+									feedback={feedback}
+									handleDelete={deleteFeedback}
+								/>
+							</>
+						}></Route>
+
+					<Route path='/about' element={<About />} />
+				</Routes>
 			</div>
-		</>
+		</Router>
 	);
 }
 
--- feedback_app/src/pages/About.jsx
+++ feedback_app/src/pages/About.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Card from '../components/shared/Card'
 
 const About = () => {
   return (
-    <div>About</div>
+    <Card>
+     <div className="about">
+      <h1>About this project</h1>
+      <p>This is a React app to leave feedback for a product or service </p>
+      <p>Version: 1.0.0</p>
+     </div>
+    </Card>
   )
 }
 
