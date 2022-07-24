d0484ef8eda89e104718d40bd537eab5796ef7cf
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-07-25 00:13:06 +0100

    finished the app

--- feedback_app/src/components/Header.jsx
+++ feedback_app/src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = ({text, bgColor, textColor}) => {
   return (
     <header style={headerStyle}>
      <div className="container">
-      {/* <Link to='/'><h2>{text}</h2></Link> */}
+      
       <h2>{text}</h2>
       
      </div>
