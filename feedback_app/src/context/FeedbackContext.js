6a10a3783943384b0bddabf68b109145f94866fd
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-07-24 21:58:32 +0100

    done

--- feedback_app/src/context/FeedbackContext.js
+++ feedback_app/src/context/FeedbackContext.js
@@ -14,20 +14,23 @@ export const FeedbackProvider = ({ children }) => {
 
 	// fetch feedback
 	const fetchFeedback = async () => {
-		const response = await fetch('/feedback?_sort=id&_order=desc');
+		const response = await fetch(
+			'http://localhost:5000/feedback?_sort=id&_order=desc'
+		);
 		const data = await response.json();
+		console.log(data);
 		setFeedback(data);
 		setIsLoading(false);
 	};
 
 	// addFeedback
 	const addFeedback = async (newFeedBack) => {
-		const response = await fetch('/feedback', {
+		const response = await fetch('http://localhost:5000/feedback', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
 			},
-			body: JSON.stringify({ newFeedBack }),
+			body: JSON.stringify(newFeedBack),
 		});
 		const data = await response.json();
 		setFeedback([data, ...feedback]);
@@ -35,7 +38,7 @@ export const FeedbackProvider = ({ children }) => {
 	// deleteFeedback
 	const deleteFeedback = async (id) => {
 		if (window.confirm('Are you sure you want to delete?')) {
-			await fetch(`/feedback/${id}`, { method: 'DELETE' });
+			await fetch(`http://localhost:5000/feedback/${id}`, { method: 'DELETE' });
 			setFeedback(feedback.filter((item) => item.id !== id));
 		}
 	};
@@ -47,9 +50,17 @@ export const FeedbackProvider = ({ children }) => {
 		});
 	};
 	// update data
-	const updateFeedback = (id, updItem) => {
+	const updateFeedback = async (id, updItem) => {
+		const response = await fetch(`http://localhost:5000/feedback/${id}`, {
+			method: 'PUT',
+			headers: {
+				Content: 'application/json',
+			},
+			body: JSON.stringify(updItem),
+		});
+		const data = await response.json();
 		setFeedback(
-			feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+			feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
 		);
 	};
 	return (
