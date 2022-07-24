28a4f897a8af613c7ba4a656a7cd5c5a4e5b31d9
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-07-24 17:05:15 +0100

    creating the loading state

--- feedback_app/src/context/FeedbackContext.js
+++ feedback_app/src/context/FeedbackContext.js
@@ -1,13 +1,28 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 // import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 export const FeedbackProvider = ({ children }) => {
+	const [isloading, setIsLoading] = useState(true);
 	const [feedback, setFeedback] = useState([]);
 	const [feedbackEdit, setFeedbackEdit] = useState({
 		item: {},
 		edit: false,
 	});
+	useEffect(() => {
+		fetchFeedback();
+	}, []);
+
+	// fetch feedback
+	const fetchFeedback = async () => {
+		const response = await fetch(
+			'http://localhost:5000/feedback?_sort=id&_order=desc'
+		);
+		const data = await response.json();
+		setFeedback(data);
+		setIsLoading(false);
+	};
+
 	// addFeedback
 	const addFeedback = (newFeedBack) => {
 		// newFeedBack.id = uuidv4();
